57c30d821f41c1230dd4decfdda6a1b12db0cef1
Alisson Ninomiya <53145093+alisnino@users.noreply.github.com>
2024-06-13 20:01:10 +0900

    Added lint, prettier rules; added chakraui and react-icons to frontend

---
+++ apps/frontend/app/styles/theme.ts
@@ -0,0 +1,18 @@
+import { extendBaseTheme } from "@chakra-ui/react"
+
+export const appTheme = extendBaseTheme({
+  colors: {
+    green: {
+      700: "#086D74",
+      800: "#004D52",
+    },
+    gray: {
+      50: "#F9F9F9",
+      100: "#EDEDED",
+      200: "#D8D8D8",
+      300: "#BEBEBE",
+      700: "#6D6D6D",
+      800: "#525252",
+    },
+  },
+})
